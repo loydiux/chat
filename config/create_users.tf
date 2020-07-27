b49e9ed95d3d0fbb94287ee6bd054a6ba214052c
Loyda <loydiux@users.noreply.github.com>
2020-07-27 16:41:42 -0500

    Add files via upload

---
+++ config/create_users.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+    region= "eu-central-1"
+    shared_credentials_file= "~/.aws/credentials"
+}
+
+
+resource "aws_iam_group_membership" "team" {
+  name = "s3-admin"
+
+  users = [
+    "${aws_iam_user.dev.name}",
+    "${aws_iam_user.prod.name}",
+  ]
+
+  group = aws_iam_group.s3_group.name
+}
+
+
+data "aws_iam_policy" "policy" {
+ arn= "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_group" "s3_group" {
+  name = "s3-access"
+    path = "/users/"
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  groups = [aws_iam_group.s3_group.name]
+  policy_arn = "${data.aws_iam_policy.policy.arn}"
+}
+
+resource "aws_iam_user" "dev" {
+  name = "super-dev"
+}
+
+resource "aws_iam_user" "prod" {
+  name = "super-prod"
+}
---
+++ config/dev_bucket_config.tf
@@ -0,0 +1,77 @@
+resource "aws_s3_bucket" "Dev"{
+    bucket = "super-develop-bucket"
+    acl = "private"
+    versioning {
+    enabled = true
+  }
+
+policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Id": "Policy1591297701977",
+    "Statement": [
+        {
+            "Sid": "Stmt1591297700192",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::084827403662:user/super-dev"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::super-develop-bucket/*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket" "Staging"{
+    bucket = "super-staging-bucket"
+    acl = "private"
+    versioning {
+    enabled = true
+  }
+
+policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Id": "Policy1591297701977",
+    "Statement": [
+        {
+            "Sid": "Stmt1591297700192",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::084827403662:user/super-dev"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::super-staging-bucket/*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket" "Testing"{
+    bucket = "super-testing-buck"
+    acl = "private"
+    versioning {
+    enabled = true
+  }
+policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Id": "Policy1591297701977",
+    "Statement": [
+        {
+            "Sid": "Stmt1591297700192",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::084827403662:user/super-dev"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::super-testing-buck/*"
+        }
+    ]
+}
+POLICY
+}
+
